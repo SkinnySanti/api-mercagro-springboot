453c93bc5511ef4429b520149f29f03335a5a7b1
“Santiago” <santiagoperez1226777@gmail.com>
2026-02-11 20:44:38 -0500

    Feat | Se crean las entidades Rol y Usuario para futura implementación de Security

---
+++ src/main/java/com/santiago/apimercagro/model/Rol.java
@@ -0,0 +1,32 @@
+package com.santiago.apimercagro.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "rol")
+public class Rol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_rol")
+    private Long idRol;
+
+    @NotBlank
+    @Column(nullable = false, unique = true, length = 50)
+    private Roles nombre;
+
+    @Column(length = 100)
+    private String descripcion;
+
+    @OneToMany(mappedBy = "rol")
+    private List<Usuario> usuarios;
+}
